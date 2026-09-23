670425fee81152d09e2b0cfaad53212fb3cdbc34
Ryan <rkim@greenwood.app>
2026-09-22 21:08:30 -0700

    fix: build rpc env action as commonjs

--- actions/rpc-env/tsup.config.ts
+++ actions/rpc-env/tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
-	format: ["esm"],
+	format: ["cjs"],
+	outExtension: () => ({ js: ".js" }),
 	target: "node24",
 	platform: "node",
 	bundle: true,
